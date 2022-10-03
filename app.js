fbfd7d2f5badab18800a4f3b9c87d14998f18191
Christian Lisangola <jochri3@gmail.com>
2022-10-03 16:05:38 +0300

    use innerHTML

--- app.js
+++ app.js
@@ -1,4 +1,21 @@
-const technos = document.querySelectorAll("li");
-// console.log(technos);
+const framworks = [
+  "Nest.js",
+  "Spring",
+  "Asp Dotnet Core",
+  "Ruby On Rails",
+  "Angular",
+  "Symfony",
+  "Laravel",
+  "Django",
+  "Express.js",
+  "Meteor",
+];
 
-// const mainTitle = document.querySelector(".title");
+const technos = document.querySelector(".technos");
+
+technos.innerHTML = "";
+
+for (let framwork of framworks) {
+  //   technos.innerHTML += `<li>${framwork}</li>`;
+  technos.innerHTML = technos.innerHTML + `<li>${framwork}</li>`;
+}
