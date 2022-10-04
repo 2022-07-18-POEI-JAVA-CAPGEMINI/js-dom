696b1e590c3e79dd3de3ac369b0214a9dae9635b
Christian Lisangola <jochri3@gmail.com>
2022-10-04 16:20:25 +0300

    disable button

--- app.js
+++ app.js
@@ -1,65 +1,34 @@
-const technos = document.querySelectorAll(".tech");
+const todos = [
+  "Text a friend I haven't talked to in a long time",
+  "Plan a vacation I've always wanted to take",
+  "Take cat on a walk",
+  "Go to the gym",
+  "Have a photo session with some friends",
+  "Learn calligraphy",
+  "Learn Express.js",
+  "Plan a trip to another country",
+  "Take a hike at a local park",
+  "Bake pastries for me and neighbor",
+  "Contribute code or a monetary donation to an open-source software project",
+  "Watch a classic movie",
+  "Do something nice for someone I care about",
+];
 
-for (let tech of technos) {
-  tech.addEventListener("click", function () {
-    // alert(`Vous avez cliqué sur ${this.innerText}`);
-    // alert(`Vous avez cliqué sur ${tech.innerText}`);
-    // console.log(tech.classList);//Lister toutes les classes d'un eelement
+const $addTodoButton = document.querySelector("#add");
+const $todoList = document.querySelector("#todos");
 
-    // Si la classe existe, on la retire
-    // Sinon, on la rajoute
-    // if (tech.classList.contains("clicked")) {
-    //   tech.classList.remove("clicked");
-    // } else {
-    //   tech.classList.add("clicked");
-    // }
-    // toggle(tech, "clicked");
-    tech.classList.toggle("clicked");
-  });
-}
+let currentIndex = 0;
 
-// function toggle(element, className) {
-//   if (element.classList.contains(className)) {
-//     element.classList.remove(className);
-//   } else {
-//     element.classList.add(className);
-//   }
-// }
-
-// Generate random heading
-const titles = ["Welcome", "Goodbye", "Good morning", "Blablabla"];
-const styles = ["style1", "style2", "style3"];
-const mainTitle = document.querySelector("h1");
-
-const titleGenerateButton = document.querySelector("button");
-
-titleGenerateButton.addEventListener("click", function (e) {
-  console.log(e);
-  const randomIndexTitle = generateRandomIndex(titles.length);
-  const randomIndexStyles = generateRandomIndex(styles.length);
-  mainTitle.innerText = titles[randomIndexTitle];
-
-  for (let style of styles) {
-    if (mainTitle.classList.contains(style)) {
-      mainTitle.classList.remove(style);
-    }
+$addTodoButton.addEventListener("click", function () {
+  //   console.log(todos[currentIndex]);
+  if (currentIndex === todos.length - 1) {
+    this.toggleAttribute("disabled");
+    return;
   }
-  mainTitle.classList.add(styles[randomIndexStyles]);
+  const $todo = document.createElement("li");
+  $todo.innerText = todos[currentIndex];
+  $todoList.append($todo);
+  currentIndex++;
+
+  //   Arrêter l'ajout des todos quand on arrive à la fin de la liste
 });
-
-function generateRandomIndex(length) {
-  return Math.floor(Math.random() * length);
-}
-
-// let count = 0;
-// document.addEventListener("mousemove", function (event) {
-//     count++;
-//   console.log("x : ", event.clientX);
-//   console.log("y : ", event.clientY);
-//     console.log(count);
-// });
-
-// document.addEventListener("contextmenu", function (event) {
-//   event.preventDefault();
-//   console.log("Context menu open");
-// });
