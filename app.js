c1513b28ae5f4b28994159c3952d27233214a64d
Christian Lisangola <jochri3@gmail.com>
2022-10-05 11:33:02 +0300

    add name field validation

--- app.js
+++ app.js
@@ -16,7 +16,28 @@ const $registerForm = document.querySelector("form");
 
 $registerForm.addEventListener("submit", function (e) {
   e.preventDefault();
-  console.log(`Nom : ${$name.value}`);
-  console.log(`Email : ${$email.value}`);
-  console.log(`Password : ${$password.value}`);
+  // console.log(`Nom : ${$name.value}`);
+  // console.log(`Email : ${$email.value}`);
+  // console.log(`Password : ${$password.value}`);
+
+  if ($name.value.length === 0) {
+    $nameError.innerText = "Le nom ne peut pas être vide";
+  } else {
+    $nameError.innerText = "";
+  }
+
+  const $userRow = document.createElement("tr");
+  const $nameColumn = document.createElement("td");
+  $nameColumn.innerText = $name.value;
+
+  const $emailColumn = document.createElement("td");
+  $emailColumn.innerText = $email.value;
+
+  $userRow.append($nameColumn, $emailColumn);
+  $users.appendChild($userRow);
+
+  // 1. Ne pas ajouter d'enregistrements dans le tableaux tant que tous les champs ne sont pas remplis
+  // 2. Terminer la validation du formulaire(email, mot de passe)
+  // 3. Le mot de passe doit avoir au moins 8 caractères
+  // 4. Ajouter un bouton pour chaque ligne qui permet supprimer la ligne
 });
