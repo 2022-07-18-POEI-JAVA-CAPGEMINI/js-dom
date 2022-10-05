7b3b820a3b095196783a0d0a7693beb17ce10c7d
Christian Lisangola <jochri3@gmail.com>
2022-10-05 11:06:04 +0300

    add users table

--- app.js
+++ app.js
@@ -3,3 +3,10 @@ const $email = document.querySelector("#email");
 const $password = document.querySelector("#password");
 
 const $registerForm = document.querySelector("form");
+
+$registerForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+  console.log(`Nom : ${$name.value}`);
+  console.log(`Email : ${$email.value}`);
+  console.log(`Password : ${$password.value}`);
+});
