df8909569ea8b2f890c8f858cf8f4a3e98b9a48b
Christian Lisangola <jochri3@gmail.com>
2022-10-05 11:12:32 +0300

    update js code

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
+//Les champs du formulaire
 const $name = document.querySelector("#name");
 const $email = document.querySelector("#email");
 const $password = document.querySelector("#password");
 
+// Les paragraphes pour afficher les erreurs
+const $nameError = document.querySelector("#name_error");
+const $emailError = document.querySelector("#email_error");
+const $passwordError = document.querySelector("#password_error");
+
+//tbody
+const $users = document.querySelector("#users");
+
+// Le formulaire
 const $registerForm = document.querySelector("form");
 
 $registerForm.addEventListener("submit", function (e) {
